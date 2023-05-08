422f262e22d1ef988891cfe07468dafeaeae42f2
Miranda Hudson <mlhudson1@buffs.wtamu.edu>
2023-05-08 15:34:53 -0500

    built out fields for user database

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace LabelPrinter{
+    public class User{
+        public int UserID{get;set;}
+        public string UserName{get;set;} = string.Empty;
+        public string Password{get;set;} = string.Empty;
+        public string FirstName{get;set;} = string.Empty;
+        public string LastName{get;set;} = string.Empty;
+        public string Role{get; set;} = string.Empty;
+    }
+}
