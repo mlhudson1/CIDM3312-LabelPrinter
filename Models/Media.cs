0805641c68846baa72d195f988ece817d2cdf55d
Miranda Hudson <mlhudson1@buffs.wtamu.edu>
2023-05-08 15:42:16 -0500

    built out fields for media database

---
+++ Models/Media.cs
@@ -0,0 +1,7 @@
+namespace LabelPrinter{
+    public class Media{
+        public int MediaID{get;set;}
+        public string Name{get; set;}=string.Empty;
+        public string Description{get;set;} = string.Empty;
+    }
+}
