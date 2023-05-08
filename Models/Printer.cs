798280b73b2773c0db3e8e40ff5cccaa4f7a7307
Miranda Hudson <mlhudson1@buffs.wtamu.edu>
2023-05-08 15:40:17 -0500

    built out fields for Printer database

---
+++ Models/Printer.cs
@@ -0,0 +1,8 @@
+namespace LabelPrinter{
+    public class Printer{
+        public int PrinterID{get;set;}
+        public string Name{get;set;}=string.Empty;
+        public string Path{get;set;}=string.Empty;
+        public string Media{get;set;} = string.Empty;
+    }
+}
