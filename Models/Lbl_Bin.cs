036be9940caa128e9ff608bad07d5c7595c6e34f
Miranda Hudson <mlhudson1@buffs.wtamu.edu>
2023-05-11 09:49:28 -0500

    Added seed data Omni sharp is showing me a weird error with my context restared omni sharp still there may have to fix something

--- Models/Lbl_Bin.cs
+++ Models/Lbl_Bin.cs
@@ -4,15 +4,10 @@ using Microsoft.EntityFrameworkCore;
 namespace LabelPrinter.Models{
     public  class Lbl_Bin{
         public int Lbl_BinID{get;set;}
-        public User addUser{get;set;} = default!;
         public DateTime addDate{get;set;}
-        public bool printed{get;set;} = false;
-        public DateTime printDate{get;set;}
         public int qty{get;set;}
         public string binID{get;set;} =string.Empty;
         public string binName{get;set;} = string.Empty;
-        public DateTime dueDate{get;set;}
-        public string PrintError{get;set;} = string.Empty;
         public List<UserLbl_Bin> UserLbl_Bins{get;set;} = default!;
         
     }
--- Program.cs
+++ Program.cs
@@ -18,7 +18,10 @@ builder.Services.AddSession(options =>
 });
 
 var app = builder.Build();
-
+using(var scope = app.Services.CreateScope()){
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
