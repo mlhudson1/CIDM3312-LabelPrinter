26fcfa58473bb090e02f60fccd079edc8e13248b
Miranda Hudson <mlhudson1@buffs.wtamu.edu>
2023-05-11 20:05:38 -0500

    Lists of labels displaying

--- Pages/Labels/Lbl_Bin.cshtml.cs
+++ Pages/Labels/Lbl_Bin.cshtml.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using LabelPrinter.Models;
+
 
 namespace CIDM3312_LabelPrinter.Pages.Labels;
 
 public class Lbl_BinModel : PageModel
 {
+    private readonly LabelContext _context;
      private readonly ILogger<Lbl_BinModel> _logger;
+     public List<Lbl_Bin> Labels {get;set;}=default!;
 
-    public Lbl_BinModel(ILogger<Lbl_BinModel> logger)
+    public Lbl_BinModel(LabelContext context,ILogger<Lbl_BinModel> logger)
     {
-        _logger = logger;
+        _logger = logger;        
+        _context = context;
     }
 
     public void OnGet()
     {
-
+        Labels = _context.Lbls_Bin.ToList();
     }
 }
