95b79f98fee366f865aecc86d0703e7e53d3f24d
Miranda Hudson <mlhudson1@buffs.wtamu.edu>
2023-05-11 19:32:51 -0500

    Just realized I over complicated it

--- Models/Lbl_Bin.cs
+++ Models/Lbl_Bin.cs
@@ -8,14 +8,7 @@ namespace LabelPrinter.Models{
         public int qty{get;set;}
         public string binID{get;set;} =string.Empty;
         public string binName{get;set;} = string.Empty;
-        public List<UserLbl_Bin> UserLbl_Bins{get;set;} = default!;
-        
+        public User user{get;set;}= default!;     
     }
 
-    public class UserLbl_Bin{
-        public int Lbl_BinID {get;set;}
-        public int UserID{get;set;}
-        public Lbl_Bin Lbl_Bin {get;set;} = default!;
-        public User User{get;set;} = default!;
-    }
 }
